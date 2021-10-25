5cef0b7e8eecdcd6ebbc1b1baf051f2118240b4a
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-10-25 10:23:03 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "team" {
+  default     = "cloudgrowth"
+  description = "Cloud Growth Team"
+}
+
+variable "project" {
+  default     = "hmc-aks"
+  description = "Hybrid Multi Cloud"
+}
+
+variable "resourcegroup" {
+  default     = "cloudgrowth"
+  description = "Cloud Growth Team"
+}
