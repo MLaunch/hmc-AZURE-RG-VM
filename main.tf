03f541c0a70d01199a50a2f47e6d2ab85e2e9e30
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-10-29 17:02:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
    features {}
+    name   = "AuthorityHost"
+    values = ["AzureAuthorityHosts.AzureGovernment"]
 }
 
 # Create a resource group if it doesn't exist
