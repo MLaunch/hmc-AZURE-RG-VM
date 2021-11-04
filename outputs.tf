501283c1a9386fb7cf23e943fd6a1ba3bbafa8eb
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-11-04 12:36:42 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = azurerm_kubernetes_cluster.example.name
 }
+
+output "cluster_id" {
+  description = "AKS cluster ID."
+  value       = azurerm_kubernetes_cluster.example."API server address"
+}
