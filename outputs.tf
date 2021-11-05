a0d168fe9b4468175e23d381650aee90fb4570b9
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-11-04 20:40:41 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,7 @@ output "cluster_name" {
   value       = azurerm_kubernetes_cluster.example.name
 }
 
-
+output "kb_host" {
+  description = "Kubernetes Cluster Host"
+  value       = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
+}
