5081ddcfb4cdab597bd1b8b8e753b908eda71675
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-11-04 20:58:01 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "kb_host" {
   description = "Kubernetes Cluster Host"
   value       = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
 }
+
+output "client_certificate" {
+  description = "Kubernetes Cluster Host"
+  value       = "{base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
+}
+
+output "client_key" {
+  description = "Kubernetes Cluster Host"
+  value       = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
+}
+
+output "cluster_ca_certificate" {
+  description = "Kubernetes Cluster Host"
+  value       = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
+}
